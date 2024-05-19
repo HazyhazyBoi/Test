6651a01cea4ad0771140e05ea03f763d9ffe2f8f
HazyhazyBoi <bluearc343@gmail.com>
2024-05-19 16:50:19 +0900

    addTest2

--- Solution/src/Test1.java
+++ Solution/src/Test1.java
@@ -4,6 +4,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git!");
+		System.out.println("addTest");
+		System.out.println("addTedt2 이거 이전에는 위에께 지워졌음");
 	}
 
 }
